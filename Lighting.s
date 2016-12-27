1fdbc185c646953166b29d0681f5f3e9c72ee820
John DeBrino Jr <johndebrino@gmail.com>
2016-12-26 21:35:00 -0500

    Modified lighting for SPI usage Added end directive for Alarm

--- Lighting.s
+++ Lighting.s
@@ -17,11 +17,16 @@ MIXED_ASM_C SETL {TRUE}
 ;-----------------------------------------------------
 ;					   Equates
 GPIOE_PDDR	EQU		0x400FF114	;Address of PDDR for Port E
-
+	
+SPI_DL		EQU		0x40076006
+SPI_DH		EQU		0x40076007
+SPI_BASE	EQU		0x40076000
 ;The following equates are the hex codes for the colors
 WHITE		EQU 	0x00FFFFFF
 AMBER		EQU		0x00FFC200
 NOCOLOR		EQU		0x00000000
+	
+SHIFT		EQU  	0x08		;Shift value to shift over bytes
 	
 	
 ;-----------------------------------------------------
@@ -35,16 +40,27 @@ NOCOLOR		EQU		0x00000000
 ;					Begin Library
 			
 		EXPORT setColor
-setColor	;Sets the GPIO output of PTE PIN0 to
-			;Desired color
-			;Input: Color in R1
+setColor	;Sends 
+			;Input: Color in R0
 			;Output: None
 			;Regmod: None
-		PUSH	{R0,LR}
-		LDR		R0,=GPIOE_PDDR	;Load data output register
-		STR		R1,[R0,#0]		;Store new color
-		POP		{R0,PC}
+		PUSH	{R0-R3,LR}
+		LDR		R1,=SPI_DL	;Load data output register
+		LDR		R2,=SPI_BASE
+		
+		LDRB	R3,[R2,#0]
+		STRB	R1,[R1,#0]		;Store new color
+		NOP
+		LSRS	R0,R0,#SHIFT
+		LDRB	R3,[R2,#0]
+		STRB	R1,[R1,#0]		;Store new color
+		NOP
+		LSRS	R0,R0,#SHIFT
+		LDRB	R3,[R2,#0]
+		STRB	R1,[R1,#0]		;Store new color
+		NOP
+		POP		{R0-R3,PC}
 			
-			
+		ALIGN
 ;-----------------------------------------------------
 		END
