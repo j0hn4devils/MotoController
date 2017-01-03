cdee31ab5cb8ff69dec38f35673d279f4c8be615
John DeBrino Jr <johndebrino@gmail.com>
2017-01-03 15:13:01 -0500

    Minor text formatting

--- Controller.s
+++ Controller.s
@@ -18,21 +18,20 @@ MIXED_ASM_C SETL {TRUE}
 
 ;General
 ;Sizes are in multiples of 8 bits
-DWORD		EQU 8			;Double word size
-WORD		EQU 4			;Word size
+DWORD		EQU 	8			;Double word size
+WORD		EQU 	4			;Word size
 HWORD		EQU	2			;Half word size
-BYTE		EQU 1			;Byte size
+BYTE		EQU 	1			;Byte size
 
 
 ;						SCGC4
-SIM_SCGC4	EQU 0x40048034
-SPI0_MASK	EQU	0x00400000
+SIM_SCGC4	EQU 	0x40048034		;SCGC4 Absolute address
+SPI0_MASK	EQU	0x00400000		;Mask to enable SPI
 
 
 ;						SCGC5
 SIM_SCGC5	EQU	0x40048038	;Absolute Address of SCGC5 Module
 EN_PTE		EQU	0x00001000	;Mask to enable PORT E in SCGC5
-	;Modify previous entry to enable ports B-E for future compatability
 
 
 ;						SCGC6
@@ -57,49 +56,35 @@ NVIC		EQU	0xE000E100	;Interrupt controller base address
 PIT_PRI_POS	EQU	0x16		;Pit Priority position
 PIT_MASK	EQU	(1 << PIT_PRI_POS)	;PIT IRQ Priority mask (Roy W Melton)
 PIT_IPR		EQU 	(NVIC + 0x314) 		;IPR5 offset
-PIT_IRQ_PRI	EQU	(0 << PIT_PRI_POS)	;Set PIT Priority to 0 (highest)
-
-
-;						DAC/ADC
-DAC0_BASE	EQU	0x4003F000			;DAC0 Base
-DAT0L		EQU	DAC0_BASE
-DAT0H		EQU	(DAC0_BASE + 0x01)
-DAC0_SR		EQU	(DAC0_BASE + 0x20)
-DAC0_C0		EQU	(DAC0_BASE + 0x21)
-DAC0_C1		EQU	(DAC0_BASE + 0x22)
-DAC0_C2		EQU	(DAC0_BASE + 0x23)
-C1_DMA_DISABLE EQU	0x00			;Mask to diable DMA/buffer
-C0_DACEN_VDDREF_HP	EQU	0xC0		;Enables DAC, uses VDD as refernce
-									;voltage, and High power mode is enabled
-DATHL_LOW	EQU 0x00	;Low voltage for DATH/L
-DATH_HIGH	EQU 0x0E	;High voltage for DATH
-DATL_HIGH	EQU	0xFF	;High voltage for DATL
+PIT_IRQ_PRI	EQU	(0 << PIT_PRI_POS)	;Set PIT Priority to 0 (highest priority)
 
 
 ;						PORTE
-PORTE_BASE	EQU 0x4004C000			;Base for port E
-PCR_30_OFFSET	EQU	0x78			;Offset for PCR30
+PORTE_BASE	EQU 	0x4004C000			;Base for port E
+PCR_30_OFFSET	EQU	0x78				;Offset for PCR30
 PTE_PCR0	EQU	PORTE_BASE			;PCR 0 located at base
-PTE_PCR30	EQU (PORTE_BASE + PCR_30_OFFSET);PCR 30
-ISF_MASK	EQU 0x01000000			;Mask to clear ISF (w1c)
-ANALOG_OUT	EQU 0x00000000			;Mask to multiplex analog output
-ANALOG_MASK	EQU (ISF_MASK :OR: ANALOG_OUT)	;Mask to fully enable analog out
-GPIO_OUT	EQU 0x00000100			;Mask to muliplex GPIO Output
+PTE_PCR30	EQU 	(PORTE_BASE + PCR_30_OFFSET);PCR 30
+ISF_MASK	EQU 	0x01000000			;Mask to clear ISF (w1c)
+ANALOG_OUT	EQU 	0x00000000			;Mask to multiplex analog output
+ANALOG_MASK	EQU 	(ISF_MASK :OR: ANALOG_OUT)	;Mask to fully enable analog out
+GPIO_OUT	EQU 	0x00000100			;Mask to muliplex GPIO Output
 GPIO_MASK	EQU	(ISF_MASK :OR: GPIO_OUT)	;Mask to fully enable GPIO out
 
 
 ;						 SPI
-SPI_BASE	EQU	0x40076000
-SPI_BAUD	EQU (SPI_BASE + 0x01)
-SPI_C2		EQU (SPI_BASE + 0x02)
-SPI_C1		EQU (SPI_BASE + 0x03)
-SPI_DL		EQU	(SPI_BASE + 0x06)
-SPI_DH		EQU	(SPI_BASE + 0x07)
-SPI_C3		EQU	(SPI_BASE + 0x0B)
-C2_8BIT		EQU	0x08		;Mask for C2 to ensure 8 bit mode
-C1_EN_MSTR	EQU	0x50		;Enables SPI and initalizes as master device
-BAUD_MASK	EQU 0x00		;Mask for baud register
-EN_FIFO		EQU	0x01		;Enables 64 bit FIFO
+SPI_BASE	EQU	0x40076000		;Base for SPI0
+SPI_BAUD	EQU 	(SPI_BASE + 0x01)	;Base for Baud register
+SPI_C2		EQU 	(SPI_BASE + 0x02)	;Base for C2 register
+SPI_C1		EQU 	(SPI_BASE + 0x03)	;Base for C1 register
+SPI_DL		EQU	(SPI_BASE + 0x06)	;Base for DL register
+SPI_DH		EQU	(SPI_BASE + 0x07)	;Base for DH register
+SPI_C3		EQU	(SPI_BASE + 0x0B)	;Base for C3  register
+C2_8BIT		EQU	0x08			;Mask for C2 to ensure 8 bit mode
+C1_EN_MSTR	EQU	0x50			;Enables SPI and initalizes as master device
+BAUD_MASK	EQU 	0x00			;Mask for baud register
+EN_FIFO		EQU	0x01			;Enables 64 bit FIFO
+
+
 
 ;-----------------------------------------------------
 ; 					Define Library
@@ -120,29 +105,29 @@ initSPI
 		;Regmod: None
 
 		;Save Registers
-		PUSH	{R0-R2}			;No LR; Faster implementation
+		PUSH	{R0-R2}		;No LR; Faster implementation
 
 		;Enable module in SCGC4
-		LDR		R0,=SIM_SCGC4	;Load SCGC4 address
-		LDR		R1,=SPI0_MASK	;Load mask to enable clock
-		LDR		R2,[R0,#0]		;Load current SCGC4 value (don't disable other modules)
-		ORRS	R2,R2,R1		;Or in the mask to enable SPI0
-		STR		R2,[R0,#0]		;Store new value at SCGC4
+		LDR	R0,=SIM_SCGC4	;Load SCGC4 address
+		LDR	R1,=SPI0_MASK	;Load mask to enable clock
+		LDR	R2,[R0,#0]	;Load current SCGC4 value (don't disable other modules)
+		ORRS	R2,R2,R1	;Or in the mask to enable SPI0
+		STR	R2,[R0,#0]	;Store new value at SCGC4
 
 		;Set the BAUD rate
-		LDR		R0,=SPI_BAUD	;Load the BAUD rate register
+		LDR	R0,=SPI_BAUD	;Load the BAUD rate register
 		MOVS	R1,#BAUD_MASK	;Load the mask for desired BAUD rate
-		STRB	R1,[R0,#0]		;Store the new baud rate
+		STRB	R1,[R0,#0]	;Store the new baud rate
 
 		;Control register 1 initalizations
-		LDR		R0,=SPI_C1		;Load C1 address
+		LDR	R0,=SPI_C1	;Load C1 address
 		MOVS	R1,#C1_EN_MSTR	;Load mask to enable SPI0 as a master device
-		STRB	R1,[R0,#0]		;Store mask at C1
+		STRB	R1,[R0,#0]	;Store mask at C1
 
 		;Control register 2 initializations
-		LDR		R0,=SPI_C2		;Load C2 address
-		MOVS	R1,#C2_8BIT		;Load mask to ensure 8 bit operation
-		STRB	R1,[R0,#0]		;Store mask at C2
+		LDR	R0,=SPI_C2	;Load C2 address
+		MOVS	R1,#C2_8BIT	;Load mask to ensure 8 bit operation
+		STRB	R1,[R0,#0]	;Store mask at C2
 
 		;Restore and return
 		POP		{R0-R2}
@@ -165,7 +150,7 @@ initPITInterrupt
 		LDR		R0,=SIM_SCGC6	;Load SCGC6 base
 		LDR		R1,=EN_PIT		;Load mask to enable PIT
 		LDR		R2,[R0,#0]		;Load current SCGC6 value
-		ORRS	R2,R2,R1		;Or in the mask
+		ORRS		R2,R2,R1		;Or in the mask
 		STR		R2,[R0,#0]		;Store new SCGC6 value
 
 		;Disable timer when in debug mode
@@ -195,7 +180,7 @@ initPITInterrupt
 		STR		R1,[R0,#0]		;Store
 
 		;CH0 Interrupt condition
-		LDR 	R0,=PIT_LDVAL0	;Load LDVAL0, which is the CH0 Base
+		LDR 		R0,=PIT_LDVAL0	;Load LDVAL0, which is the CH0 Base
 		LDR		R1,=TFLG_CLR	;Load mask to reset interrupt condition
 		STR		R1,[R0,#TFLG1]	;Store mask at offset
 
